cf13fd56fb9ec4ad7e457333ae6f7ffc408d858f
Olena Diachenko <ellenka24@gmail.com>
2023-08-27 00:17:57 +0300

    hw-82 done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Carousel from "./components/Carousel.jsx";
+import firstImg from './components/images/first.jpg'
+import secondImg from './components/images/second.jpg'
+import thirdImg from './components/images/third.jpg'
 
 function App() {
-  return (
-    <div className="App">
+    const images = [firstImg, secondImg, thirdImg]
 
+    return (
+    <div className="App">
+      <Carousel images={images}/>
     </div>
   );
 }
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import cn from 'classnames';
+
+class Carousel extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: this.props.images,
+            activeImg: this.props.images[0]
+        }
+    }
+
+    onPrevHandler = () => {
+        let prevImg;
+        const prevImgIndex = this.state.images.indexOf(this.state.activeImg);
+        prevImgIndex === 0 ? prevImg = this.state.images[this.state.images.length - 1] : prevImg = this.state.images[prevImgIndex - 1]
+
+        this.setState({
+            activeImg: prevImg
+        });
+    }
+
+    onNextHandler = () => {
+        let nextImg;
+        const currentImgIndex = this.state.images.indexOf(this.state.activeImg) + 1;
+        currentImgIndex === this.state.images.length ? nextImg = this.state.images[0] : nextImg = this.state.images[currentImgIndex]
+
+        this.setState({
+            activeImg: nextImg
+        });
+    }
+
+    renderItem() {
+        const currentClass = cn("carousel-item", "active");
+        return (
+            <React.Fragment key={Math.floor(Math.random() * 100)}>
+                <div className={currentClass}>
+                    <img alt="first img" className="d-block w-100" src={this.state.activeImg}></img>
+                </div>
+            </React.Fragment>
+        )
+    }
+
+    render() {
+        return (
+            <div id="carousel" className="carousel slide container-sm" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    {this.renderItem()}
+                </div>
+                <button className="carousel-control-prev" data-bs-target="#carousel" type="button" data-bs-slide="prev" onClick={this.onPrevHandler}>
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" data-bs-target="#carousel" type="button" data-bs-slide="next" onClick={this.onNextHandler}>
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        )
+    }
+}
+
+export default Carousel;
